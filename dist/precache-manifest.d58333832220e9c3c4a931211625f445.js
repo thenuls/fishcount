a750bcb43877e950a1431fc501ca973c1054772f
Buddhika P. De Silva <bevylabs@gmail.com>
2020-06-04 17:33:39 +0530

    rest

---
+++ dist/precache-manifest.d58333832220e9c3c4a931211625f445.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb126554e33323094dcf",
+    "url": "/fishcount/css/app.851ae1fd.css"
+  },
+  {
+    "revision": "eb126554e33323094dcf",
+    "url": "/fishcount/js/app.bc75ff63.js"
+  },
+  {
+    "revision": "2864431e97291a33b06e",
+    "url": "/fishcount/css/chunk-vendors.17d67150.css"
+  },
+  {
+    "revision": "2864431e97291a33b06e",
+    "url": "/fishcount/js/chunk-vendors.431ca444.js"
+  },
+  {
+    "revision": "167cabbb437be58176019d1a73947da5",
+    "url": "/fishcount/index.html"
+  },
+  {
+    "revision": "5e48083cf0e85f642d4e4d357b1da07f",
+    "url": "/fishcount/img/bot.png"
+  },
+  {
+    "revision": "391e4fb8f4a4ec32c32ca20a566219a9",
+    "url": "/fishcount/img/legend.png"
+  },
+  {
+    "revision": "6f06b9ed9b411dba69331544766f1243",
+    "url": "/fishcount/img/type-magnified-thumb.jpg"
+  },
+  {
+    "revision": "98233e985db7e75041bfcb4e500b83d0",
+    "url": "/fishcount/img/type-magnified.jpg"
+  },
+  {
+    "revision": "dc002240608f3369a8aefff1777f0d89",
+    "url": "/fishcount/img/type-microscope-thumb.jpg"
+  },
+  {
+    "revision": "369ff031461212415351e5aa09200099",
+    "url": "/fishcount/img/type-microscope.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fishcount/robots.txt"
+  },
+  {
+    "revision": "1f5362cb0f8d4eab1f10205ae869a453",
+    "url": "/fishcount/opencv.js"
+  },
+  {
+    "revision": "d412e6f6a457fdb093713651db73169e",
+    "url": "/fishcount/logo.ico"
+  },
+  {
+    "revision": "a61d34afc155c2b9c523cc8a1acbfb86",
+    "url": "/fishcount/img/bot.gif"
+  }
+];
