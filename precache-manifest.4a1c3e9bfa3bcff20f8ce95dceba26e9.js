f66b6a844d851fd385f9eb045acbbc473a7371c5
Buddhika P. De Silva <bevylabs@gmail.com>
2020-06-04 16:39:41 +0530

    Updates

---
+++ precache-manifest.4a1c3e9bfa3bcff20f8ce95dceba26e9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "132228fad8b3458d0299",
+    "url": "/fishcount/css/app.f115216e.css"
+  },
+  {
+    "revision": "132228fad8b3458d0299",
+    "url": "/fishcount/js/app.6c35fa27.js"
+  },
+  {
+    "revision": "2864431e97291a33b06e",
+    "url": "/fishcount/css/chunk-vendors.17d67150.css"
+  },
+  {
+    "revision": "2864431e97291a33b06e",
+    "url": "/fishcount/js/chunk-vendors.431ca444.js"
+  },
+  {
+    "revision": "f51d2004919e900528eb13115fbe8833",
+    "url": "/fishcount/index.html"
+  },
+  {
+    "revision": "f2a4f87e90d5e2606f2fb582cfbea5ad",
+    "url": "/fishcount/css/chunk-vendors.4270142f.css"
+  },
+  {
+    "revision": "5e48083cf0e85f642d4e4d357b1da07f",
+    "url": "/fishcount/img/bot.png"
+  },
+  {
+    "revision": "a61d34afc155c2b9c523cc8a1acbfb86",
+    "url": "/fishcount/img/bot.gif"
+  },
+  {
+    "revision": "391e4fb8f4a4ec32c32ca20a566219a9",
+    "url": "/fishcount/img/legend.png"
+  },
+  {
+    "revision": "6f06b9ed9b411dba69331544766f1243",
+    "url": "/fishcount/img/type-magnified-thumb.jpg"
+  },
+  {
+    "revision": "98233e985db7e75041bfcb4e500b83d0",
+    "url": "/fishcount/img/type-magnified.jpg"
+  },
+  {
+    "revision": "dc002240608f3369a8aefff1777f0d89",
+    "url": "/fishcount/img/type-microscope-thumb.jpg"
+  },
+  {
+    "revision": "369ff031461212415351e5aa09200099",
+    "url": "/fishcount/img/type-microscope.jpg"
+  },
+  {
+    "revision": "6851905af3954e586aa3a73501f6bc53",
+    "url": "/fishcount/js/app.17c39733.js"
+  },
+  {
+    "revision": "d412e6f6a457fdb093713651db73169e",
+    "url": "/fishcount/logo.ico"
+  },
+  {
+    "revision": "0f872ed6a54d3a2ed067efec7aa8b1c5",
+    "url": "/fishcount/precache-manifest.c34d79a2e8a8f1ad6f19fbbd7a9f7113.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/fishcount/robots.txt"
+  },
+  {
+    "revision": "29db7ab2101bce49ac10da5771a1a1c1",
+    "url": "/fishcount/service-worker.js"
+  },
+  {
+    "revision": "4194fbf781e77270da94cce22cb09029",
+    "url": "/fishcount/js/chunk-vendors.3904fad3.js"
+  },
+  {
+    "revision": "8f9a45b13ada77d6b0f68f1a4efb29c9",
+    "url": "/fishcount/opencv.js"
+  }
+];
